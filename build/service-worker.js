702cda6371155e998c5e5b07fc52f143592b4f55
sidoroval163 <60754312+sidoroval163@users.noreply.github.com>
2020-09-01 11:01:57 +0300

    fix:resolution

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.567804a502af310dadf4b9dc898b8a0b.js"
+  "/precache-manifest.d2a2e2ecd81a8296a85d7f6bf758bf6e.js"
 );
 
 self.addEventListener('message', (event) => {
